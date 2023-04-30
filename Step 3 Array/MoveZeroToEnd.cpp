3e84f19e1f9eb4133589d82e1f77a44b8bec7a94
Ajay Kushwaha <ak383834@gmail.com>
2023-04-30 14:38:41 +0530

    two another methods of solving this problem added

--- Step 3 Array/MoveZeroToEnd.cpp
+++ Step 3 Array/MoveZeroToEnd.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//gfg
 void pushZerosToEnd(int arr[], int n) {
 	   int left = 0, right = 0;
         while (right < n) {
@@ -13,3 +15,35 @@ void pushZerosToEnd(int arr[], int n) {
             right++;
         }
 }
+
+
+//LeetCode
+//Method 1
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int count=0;
+       for(auto it=nums.begin();it!=nums.end();){
+           if(*it==0){
+               it = nums.erase(it);
+               count++;
+           } else {
+               it++;
+           }
+       }
+       nums.insert(nums.end(),count,0);
+    }
+};
+
+//Method 2
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = count_if(nums.begin(), nums.end(), [](int n){ return n == 0; });
+        nums.erase(remove(nums.begin(),nums.end(),0),nums.end());
+        nums.insert(nums.end(),count,0);
+    }
+};
+
+
+
