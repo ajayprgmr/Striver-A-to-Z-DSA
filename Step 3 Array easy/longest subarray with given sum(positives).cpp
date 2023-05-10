283027dfdb3e28904ca3138e0a14e88840683e5d
Ajay Kushwaha <ak383834@gmail.com>
2023-05-10 19:45:13 +0530

    All the problem of array - easy one is completed now

--- Step 3 Array easy/longest subarray with given sum(positives).cpp
+++ Step 3 Array easy/longest subarray with given sum(positives).cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 //bruteforce approach time complexity big-oh of n^3 its like generating all the substring and checking 
+// This solution works all types of inputs whether a[i] is >0 or =0 or <0  but this is bruteforce.
 int maxsubarrayposk(vector<int>&v, int target){
     int best=0;
     int n=v.size();
@@ -12,7 +13,7 @@ int maxsubarrayposk(vector<int>&v, int target){
             for(int k=i;k<=j;k++){ 
                 sum=sum+v[k]; 
             }
-            if(sum==target)    // after summing the subarray from i-------------j checking, it's equal to target or not.
+            if(sum==target)    // after summing the subarray from [i-------------j] checking, it's equal to target or not.
              best=max(best,j-i+1);
         }
     }
@@ -20,7 +21,7 @@ int maxsubarrayposk(vector<int>&v, int target){
 
 }
 
-// second approach after bruteforce  is in O(n**2)
+// second approach after bruteforce  is in O(n**2) but wait this is also works for all types of int input
 int maxsubarrayposk(vector<int>&v, int k){
     int best=0;
     int n=v.size();
@@ -35,6 +36,25 @@ int maxsubarrayposk(vector<int>&v, int k){
     return best;
 
 }
+// this is optimal solution using two pointer approach
 
-// optimal solution which is O(n) time complexity
+int maxsubarrayposk(vector<int>&v, int k){
+   int best=0, left=0, right=0 ,sum=0, n=v.size();
+    while( right < n){
 
+        sum=sum+v[right];
+        if(sum==k)
+        best=max(best,right-left+1);
+
+        while(left<=right && sum>k){
+            sum=sum-v[left];
+            if(sum==k)
+            best=max(best,right-left+1);
+            left++;
+        }
+
+        right++;
+    }             
+    return best;
+
+}
