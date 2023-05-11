86ef9c4fe8de815e9078674532492c6e0d434894
Ajay Kushwaha <ak383834@gmail.com>
2023-05-11 20:36:37 +0530

    sorting using three pointers when only three types of elements are there

---
+++ Step 3 Array Medium/Sort an array of 0's 1's and 2's .cpp
@@ -0,0 +1,44 @@
+// Bruteforce thinking but yeah still TC  is  big-oh of n only 
+#include<bits/stdc++.h>
+using namespace std;
+void sort0s1s2s(vector<int>&nums){
+    int zeros= 0;
+    int ones= 0;
+    int twos= 0;
+
+    for(int i=0; i< nums.size() ; i++){
+        if(nums[i]==0) zeros++;
+        else if(nums[i]==1) ones++;
+        else twos++;
+    }
+    for(int i=0; i<nums.size(); i++) {
+
+        if(i<zeros)
+        nums[i]=0;
+        else if(i>=zeros && i<ones+zeros) 
+             nums[i]=1;
+             else
+             nums[i]=2;
+    }
+
+}
+// Another approach using threee pointer in which we can do this in a single loop
+void sortArray(vector<int>& arr, int n) {
+
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {  // play only on mid
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {   // playing only on mid
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
