73154dd1750b1b4b280c038457c8ad58c34de947
Ajay Kushwaha <ak383834@gmail.com>
2023-08-06 12:42:35 +0530

    missing and repeating in array solved

--- Array--hard/Find the Repeating and Missing Numbers.cpp
+++ Array--hard/Find the Repeating and Missing Numbers.cpp
@@ -40,7 +40,7 @@ vector<int> findMissingRepeatingNumbers(vector<int> a) {
     return {repeating, missing};
 }
 
-// Better TC : O(2N)  SC: O(N)
+// Better Solution TC : O(2N)  SC: O(N)
 vector<int> findMissingRepeatingNumbers(vector<int> a) {
     int n = a.size(); // size of the array
     int hash[n + 1] = {0}; // hash array
@@ -62,7 +62,36 @@ vector<int> findMissingRepeatingNumbers(vector<int> a) {
     return {repeating, missing};
 }
 
+//  Optimal Solution using Mathematics and TC : O(N) | SC: O(1)
+vector<int> findMissingRepeatingNumbers(vector<int> a) {
+    long long n = a.size(); // size of the array
 
-//optimal Solution O(N) | SC: O(n)
+    // Find Sn and S2n:
+    long long SN = (n * (n + 1)) / 2;
+    long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+    // Calculate S and S2:
+    long long S = 0, S2 = 0;
+    for (int i = 0; i < n; i++) {
+        S += a[i];
+        S2 += (long long)a[i] * (long long)a[i];
+    }
+
+    //S-Sn = X-Y:
+    long long val1 = S - SN;
+
+    // S2-S2n = X^2-Y^2:
+    long long val2 = S2 - S2N;
+
+    //Find X+Y = (X^2-Y^2)/(X-Y):
+    val2 = val2 / val1;
+
+    //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+    // Here, X-Y = val1 and X+Y = val2:
+    long long x = (val1 + val2) / 2;
+    long long y = x - val1;
+
+    return {(int)x, (int)y};
+}
 
 
