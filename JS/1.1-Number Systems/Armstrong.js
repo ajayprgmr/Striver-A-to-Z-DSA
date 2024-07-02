6c1b10119c744ce8489bfb5a90e14e2f4703c867
Ajay Kushwaha <86186444+ajayprgmr@users.noreply.github.com>
2024-07-02 21:42:27 +0530

    Update Armstrong.js
    GFG Link added

--- JS/1.1-Number Systems/Armstrong.js
+++ JS/1.1-Number Systems/Armstrong.js
@@ -1,6 +1,7 @@
 /*Def : An Armstrong number, also known as a narcissistic number, is a number that is equal to the sum of its own digits raised to the power of the number of digits.
 E.g.
 abc...z = a^n + b^n + c^n + ... + z^n
+GFG : https://www.geeksforgeeks.org/problems/armstrong-numbers2727/1
 */
 function armStrongNumber(n) {
   let temp = n
